6d3bceb14508203e5bbfe5496c20fb5189fc6126
‘Dswee’ <dieich@outlook.com>
2022-09-09 22:47:00 +0800

    Site updated: 2022-09-09 22:46:54

---
+++ my_script/js/water_wave.js
@@ -0,0 +1,226 @@
+
+function getimage(gl, url){
+    var img = url;
+    var image = new Image();
+    
+    const tex = gl.createTexture();
+    image.onload = function() {
+    //文件的Base64字符串
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,gl.RGB, gl.UNSIGNED_BYTE,
+            image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+    
+    image.src = url;
+
+/**
+ * 图像转Base64
+ */
+    return tex;
+    let c = document.getElementById("img_load");
+    let ctx = c.getContext("2d");
+    var im = ctx.getImageData(0,0,512,512);
+    return im.data;
+}
+const canvas = document.getElementById("glCanvas");
+const gl = canvas.getContext('webgl');
+const shader_program_wave = init_shader(gl, vs, wave_fs);
+const shader_program = init_shader(gl, vs, fs);
+
+var time = 0.0;
+var vbo = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+gl.useProgram(shader_program_wave);
+var vp = gl.getAttribLocation(shader_program_wave, 'aVertexPosition');
+var cnt_pos = gl.getUniformLocation(shader_program_wave, 'cnt');
+var m_pos = gl.getUniformLocation(shader_program_wave, "m_pos");
+var mouse_down = gl.getUniformLocation(shader_program_wave, "mouse_down");
+var preframe = gl.getUniformLocation(shader_program_wave, "preFrame");
+var clear_pos = gl.getUniformLocation(shader_program_wave, "clear");
+gl.uniform1i(preframe, 0);
+
+
+gl.useProgram(shader_program);
+var vp = gl.getAttribLocation(shader_program, 'aVertexPosition');
+var waveframe = gl.getUniformLocation(shader_program, "wave");
+var background_image = gl.getUniformLocation(shader_program, "image");
+gl.uniform1i(waveframe, 0);
+gl.uniform1i(background_image, 1);
+
+const frame = [
+    -1.0, -1.0,
+    1.0, -1.0, 
+    -1.0, 1.0, 
+    1.0, 1.0
+];
+gl.bufferData(gl.ARRAY_BUFFER,
+    new Float32Array(frame),
+    gl.STATIC_DRAW);
+
+gl.vertexAttribPointer(
+        vp,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0);
+gl.enableVertexAttribArray(
+        vp
+);
+var scene_scalex = 2.0;
+var scene_scaley = 2.0;
+var scene_centerx = 0.0;
+var scene_centery = 0.0;
+var is_mouse_dowm = false;
+var px = 0;
+var py = 0;
+var mouse_downf = 0.0;
+var clear = 1.0;
+
+var texturebuffers = [];
+
+type = gl.getExtension('OES_texture_float') == null? gl.UNSIGNED_BYTE: gl.FLOAT;
+
+const btex = getimage(gl, "../img/stones.png");
+//function gentex(gl, url){
+    /*const btex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, btex);
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 512;
+    const height = 512;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    //const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+    //gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+    //            width, height, border, srcFormat, srcType,
+    //            pixel);
+    //const pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
+    //gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+    var ctx = canvas.getContext("2d");
+    
+    const pixels = getimage('../img/stones.png');
+    alert(pixels);
+    //const image = new Image();
+    //image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, btex);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+        width, height, border, srcFormat, srcType,
+        pixels);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.bindTexture(gl.TEXTURE_2D, null);*/
+  //};
+  //image.src = url;
+  //return texture;
+//}
+//var btex = gentex(gl, './img/stones.png');
+
+for (let i = 0; i < 2; i++){
+    texturebuffers.push(gl.createTexture());
+    gl.bindTexture(gl.TEXTURE_2D, texturebuffers[i]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 512, 512, 0, gl.RGB, type, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    
+}
+
+var buffer_id = 0;
+
+var wave_buffer = gl.createFramebuffer();
+
+function draw(){
+    gl.useProgram(shader_program_wave);
+    gl.uniform1f(cnt_pos, time);
+    gl.uniform2f(m_pos, px, py);
+    gl.uniform1f(mouse_down, mouse_downf);
+    gl.uniform1f(clear_pos, clear);
+    clear = 0.0;
+    time++;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, wave_buffer);
+    gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, texturebuffers[1-buffer_id]);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texturebuffers[buffer_id], 0);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.useProgram(shader_program);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, texturebuffers[buffer_id]);
+    gl.activeTexture(gl.TEXTURE1);
+	gl.bindTexture(gl.TEXTURE_2D, btex);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    requestAnimationFrame(draw);
+    buffer_id = 1 - buffer_id;
+}
+
+function use_shader(){
+    time = 0;
+    const shader_program = reload_shader(gl);
+    vp = gl.getAttribLocation(shader_program, 'aVertexPosition');
+    cnt_pos = gl.getUniformLocation(shader_program, 'cnt');
+    scale_loc = gl.getUniformLocation(shader_program, "scale");
+    center_loc = gl.getUniformLocation(shader_program, "center");
+    gl.useProgram(shader_program);
+}
+
+canvas.addEventListener("mousedown", function(e){
+    if(e.button == 0){
+        is_mouse_dowm = true;
+        px = e.offsetX / 512.0 ;
+        py = (1.0 - e.offsetY / 512.0);
+        mouse_downf = 1.0;
+    }
+});
+canvas.addEventListener("mouseup", function(e){
+    if(e.button == 0){
+        is_mouse_dowm = !is_mouse_dowm;
+        mouse_downf = 1.0 - mouse_downf;
+    }
+});
+canvas.addEventListener("mousemove", function(e){
+    if(e.button == 0 && is_mouse_dowm == true){
+        px = e.offsetX / 512.0 ;
+        py = (1.0 - e.offsetY / 512.0);
+    }
+});
+
+function getBase64Image(img) {
+    var ca = document.createElement("canvas");
+    ca.width = img.width;
+    ca.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+    var dataURL = ca.toDataURL("../img" + ext);
+    return dataURL;
+}
+
+/**
+ *Base64字符串转二进制
+ */
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {
+        type: mime
+    });
+}
+
+requestAnimationFrame(draw);
+
---
+++ my_script/js/wave_shader.js
@@ -0,0 +1,110 @@
+var vs = `
+attribute vec4 aVertexPosition;
+precision highp float;
+void main() { 
+	gl_Position = aVertexPosition;
+}
+`;
+
+var wave_fs = `
+precision highp float;
+uniform float cnt;
+uniform vec2 m_pos;
+uniform float mouse_down;
+uniform sampler2D preFrame;
+uniform float clear;
+vec2 wave_shader(vec2 uv){
+	float offset = 1.0 / 512.0;
+	vec4 d0 = texture2D(preFrame, uv);// * 2.0 - 1.0;
+
+	vec4 d1 = texture2D(preFrame, uv + vec2(0.0, 1.0) * offset) ;//* 2.0 - 1.0;
+	vec4 d2 = texture2D(preFrame, uv + vec2(1.0, 0.0) * offset);// * 2.0 - 1.0;
+	vec4 d3 = texture2D(preFrame, uv + vec2(0.0, -1.0) * offset);// * 2.0 - 1.0;
+	vec4 d4 = texture2D(preFrame, uv + vec2(-1.0, 0.0) * offset) ;//* 2.0 - 1.0;
+	float d = length(gl_FragCoord.xy - 512.0 * m_pos);
+	d = mouse_down * smoothstep(4.5, 0.5, d);
+	//d *= sin(1.0 * cnt / 60.0 * 8.0) * 0.05;
+	d += 2.0 * d0.x - d0.y + 0.5*(d1.x + d2.x + d3.x + d4.x - 4.0 * d0.x);
+	d *= 0.999;
+	return vec2(d, d0.x) * (1.0 - clear);
+}
+void main(){
+    vec2 c = wave_shader(gl_FragCoord.xy/512.0);
+    //c = 0.5 * c + 0.5;
+	gl_FragColor = vec4(vec3(c.x, c.y,0), 1.0);
+}
+`;
+
+var fs = `
+precision highp float;
+uniform sampler2D wave;
+uniform sampler2D image;
+int mod(int a, int b){
+	if(a < 0) a = -a;
+	if(b < 0) b = -b;
+	return a - a / b * b;
+}
+int xor(int a, int b){
+	if(a == b) return 0;
+	else return 1;
+}
+void main(){
+    float c = texture2D(wave, gl_FragCoord.xy/512.0).x;
+    float cx = texture2D(wave, gl_FragCoord.xy/512.0 + vec2(1.0, 0.0) / 512.0).x;
+    float cy = texture2D(wave, gl_FragCoord.xy/512.0 + vec2(0.0, 1.0) / 512.0).x;
+    vec2 n = vec2(cx - c, cy - c);
+    vec2 v = gl_FragCoord.xy/512.0 + 0.5 * n;
+    vec3 col = texture2D(image, v).rgb;
+    gl_FragColor = vec4(col, 1.0);
+    //int r = mod(int(v.x), 2);
+	//int b = mod(int(v.y), 2);
+	//r = xor(r, b);
+	//gl_FragColor = vec4(r, r, 1.0, 1.0);
+}
+`;
+
+function reload_shader(gl){
+    vs = document.getElementById('vertex shader').value;
+    fs = document.getElementById('fragment shader').value;
+    return init_shader(gl);
+}
+
+function init_shader(gl, vshader, fshader){
+    const vertex_shader = shader_load(gl, gl.VERTEX_SHADER, vshader);
+    const frag_shader = shader_load(gl, gl.FRAGMENT_SHADER, fshader);
+
+    const p = gl.createProgram();
+    gl.attachShader(p, vertex_shader);
+    gl.attachShader(p, frag_shader);
+    gl.linkProgram(p);
+
+  // If creating the shader program failed, alert
+
+    if (!gl.getProgramParameter(p, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return p;
+}
+function shader_load(gl, type, source){
+    const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+    gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+    gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
